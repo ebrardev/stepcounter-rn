4f876d53d43628210a7d71d58bd216281138c23c
ebrardev <16155052@mersin.edu.tr>
2024-01-10 20:04:11 +0300

    states

--- App.js
+++ App.js
@@ -1,7 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState,useEffect,useRef} from 'react';
+import { StyleSheet, Text, View,TouchableOpacity,SafeAreaView } from 'react-native';
+import { Accelerometer } from 'expo-sensors';
+import { Constants } from 'expo-constants';
+import LottieView from 'lottie-react-native';
 
 export default function App() {
+
+  const [steps,setSteps] = useState(0)
+  const [isWalking,setIsWalking] = useState(false)
+  const [lastY,setLastY] = useState(0)
+  const [lastTimeStamp,setLastTimeStamp] = useState(0)
+
+  const animationRefRunning = useRef(null)
+  const animationRefSitting = useRef(null)
+
+  useEffect(()=>{
+    let subscription;
+    Accelerometer.isAvailableAsync().then((result) => {
+      if (result) {
+        subscription = Accelerometer.addListener((accelerometer) => {
+          const {y} = accelerometer;
+          const thresold = 0.1;
+          const timeStamp = new Date().getTime();
+
+          if(
+            Math.abs(y-lastY) > thresold &&
+            !isWalking && (timeStamp - lastTimeStamp) > 800
+          ) {
+            setIsWalking(true)
+            setLastY(y)
+            setLastTimeStamp(timeStamp)
+
+            setSteps((prevSteps)=>prevSteps+1)
+
+            setTimeout(() =>{
+              setIsWalking(false)
+            })
+          }
+          
+        })
+      }
+    })
+  }, [])
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
